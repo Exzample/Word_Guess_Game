554654d9012e9d527dede8ff4b9e69e018fb4505
Doug Admin <dougadmin28@DatafileTechnologies.local>
2019-03-14 17:24:08 -0500

    remove all this when you figure it out

--- JS/main.js
+++ JS/main.js
@@ -24,14 +24,141 @@ function printArray(Arr){
     console.log("---------")
 };
 
+/*
 //I think I may need to convert randomAnimal to string//
 
 
+// this function is currently assigned to the OnLoad event of the body of the document.  So when the OnLoad event fires off
+// for the body of the html document, the code in this function will automatically execute.
+    function onLoad() {
+        console.log("onLoad...");
+        var textGuess = document.getElementById('txtGuess'); //creating a variable for the text of the guess(pranay)
+        currentSecretWord = getSecretWord();
+        document.getElementById('hint').innerHTML = "<p>" + currentSecretWord.hint + "</p>";
+        player.guesses = MAX_ATTEMPTS;
+        //make the value of textGuess null(in case it has a value from previous uses of OnLoad function)
+        textGuess.value = '';
+        // Focus back on the text input for the next question. --Korey
+        textGuess.focus();
+
+    function checkWord() {
+        console.log("checkWord...")
+        var word = document.getElementById('txtGuess').value;
+
+        if (word.toLowerCase() == currentSecretWord.word) {
+            updateScore(true);
+            displayResult(true);
+
+            // Remove that question and go to another question.
+            // Also, reset the text field to be blank. --Korey
+            var correctIndex = secretWords.indexOf(currentSecretWord);
+            var correctAnswer = secretWords.splice(correctIndex, 1);
+            correctWords.push(correctAnswer);
+
+            document.getElementById('txtGuess').value = "";
+
+        } else {
+            updateScore(false);
+            displayResult(false);
+
+            // After wrong answer, focus and select the text in the text field. --Korey
+            document.getElementById('txtGuess').select();
+            }
+            onLoad();
+        }
+
+        function updateScore(result) {
+        // display score div if not yet displayed
+            if (scoreLoaded == false) {
+                loadScoreDiv();
+                scoreLoaded = true;
+            }
+
+        function loadScoreDiv() {
+            var scoreDiv = document.getElementById('scorebox');
+            var topOffset = -60; //push the element above the viewport
+            scoreDiv.style.top = topOffset;
+            scoreDiv.style.display = "block";
+            var loadScore = setInterval(function () {
+                topOffset += 1;
+                scoreDiv.style.top = topOffset + "px";
+                if (topOffset >= 0) {
+                    clearInterval(loadScore);
+                }
+            }, 5);
+        }
+
+
+        // logic for correct answer
+        if (result) {
+            player.score++;
+        }
+        // logic for incorrect answer
+        else {
+            player.guesses -= 1;
+            if (player.guesses <= 0) {
+                player.finalScore = player.score;
+                player.score = 0;
+            }
+        }
+        document.getElementById('score').innerHTML = "<p>Score: " + player.score + "</p>";
+    }
+    function displayResult(result) {
+        var resultDiv = document.getElementById('resultbox');
+        var resultDivText = document.getElementById('resultText');
+        var guessesDivText = document.getElementById('guessesText');
+        var submitButton = document.getElementById('submit');
+        var restartButton = document.getElementById('restart');
+        resultDiv.style.display = 'block';
+        submitButton.disabled = true;
+        if (result) {
+            resultDivText.innerHTML = 'Correct!';
+            resultDiv.style.backgroundColor = 'rgba(0, 255, 0, 0.7)';
+
+        } else {
+            if (player.guesses > 0) {
+                resultDivText.innerHTML = 'Incorrect!';
+                guessesDivText.innerHTML = 'You have ' + player.guesses + ' guesses left.';
+                resultDiv.style.backgroundColor = 'rgba(255, 0, 0, 0.7)';
+            } else {
+                resultDivText.innerHTML = 'GAME OVER';
+                guessesDivText.innerHTML = 'Final Score: ' + player.finalScore;
+                restartButton.style.display = "inline";
+                return;
+            }
+        }
+            setTimeout(function () {
+                resultDivText.innerHTML = '';
+                guessesDivText.innerHTML = '';
+                resultDiv.style.display = 'none';
+                submitButton.disabled = false;
+                document.getElementById('txtGuess').value = '';
+            }, 2000);
+        }
+
+        function restartGame() {
+            var resultDiv = document.getElementById('resultbox');
+            var submitButton = document.getElementById('submit');
+            var restartButton = document.getElementById('restart');
+            resultDiv.style.display = 'none';
+            submitButton.disabled = false;
+            restartButton.style.display = 'none';
+
+            resetSecretWord(secretWords, correctWords);
+
+            resetPlayer();
+            onLoad();
+        }
+
+        var resetSecretWord = function (mainArray, tempArray) {
+            for (var i = 0; i < tempArray.length; i++) {
+                mainArray.push(tempArray[i][0]);
+            }
+            tempArray.length = 0;
+        }
 
 
 
-
-/*
 $(document).ready(function () {
     //This may help register the user guess
     this.onkeyup = function(event) {
